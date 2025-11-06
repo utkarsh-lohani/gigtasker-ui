cc282b3621c722eaec05a8fc1baf5ef82251881c
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-11-06 19:38:32 +0530

    - Improved relationship between dashboard and tasklist. Now data is fetched only once and won't be refetched unless a new task is created

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, QueryList, signal, ViewChildren } from '@angular/core';
+import { Component, effect, inject, QueryList, signal, ViewChildren } from '@angular/core';
 import { ApiService } from '../core/api';
 import { toSignal } from '@angular/core/rxjs-interop';
 
@@ -8,6 +8,7 @@ import { CreateTask } from '../create-task/create-task';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TaskList } from '../task-list/task-list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { TaskDTO } from '../core/models/task.model';
 
 @Component({
     selector: 'app-dashboard',
@@ -30,13 +31,71 @@ export class Dashboard {
     // This grabs *all* <app-task-list> components in our template
     @ViewChildren(TaskList) taskLists!: QueryList<TaskList>;
 
+    // We create writable signals for our data
+    public allTasks = signal<TaskDTO[] | undefined>(undefined);
+    public myTasks = signal<TaskDTO[] | undefined>(undefined);
+
+    // We also create signals for *their* loading states
+    public allTasksLoading = signal(true);
+    public myTasksLoading = signal(true);
+
     public selectedTabIndex = signal(0);
 
+    constructor() {
+        // Use an effect() to react to tab changes
+        effect(
+            () => {
+                if (this.selectedTabIndex() === 0 && !this.allTasks()) {
+                    this.loadAllTasks();
+                }
+
+                if (this.selectedTabIndex() === 1 && this.user() && !this.myTasks()) {
+                    this.loadMyTasks();
+                }
+                // ---
+            },
+            { allowSignalWrites: true }
+        );
+    }
+
+    // This method is called by the (taskCreated) event
     public refreshTaskLists(): void {
-        if (this.taskLists) {
-            for (const element of this.taskLists) {
-                element.loadTasks();
-            }
+        // Now we just re-run the *specific* loads
+        this.loadAllTasks();
+        if (this.user()) {
+            this.loadMyTasks();
         }
+    }
+
+    // --- 6. NEW DATA-LOADING METHODS ---
+    private loadAllTasks(): void {
+        this.allTasksLoading.set(true);
+        this.apiService.getAllTasks().subscribe({
+            next: (data) => {
+                this.allTasks.set(data);
+                this.allTasksLoading.set(false);
+            },
+            error: (err) => {
+                console.error(err);
+                this.allTasksLoading.set(false);
+            },
+        });
+    }
+
+    private loadMyTasks(): void {
+        const userId = this.user()?.id;
+        if (!userId) return; // Safety check
+
+        this.myTasksLoading.set(true);
+        this.apiService.getTasksByUserId(userId).subscribe({
+            next: (data) => {
+                this.myTasks.set(data);
+                this.myTasksLoading.set(false);
+            },
+            error: (err) => {
+                console.error(err);
+                this.myTasksLoading.set(false);
+            },
+        });
     }
 }
