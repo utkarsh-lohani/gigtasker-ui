8386ce291cd5e2a1413be5171184f9d5be06065f
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-11-11 13:13:24 +0530

    - Added section to view bids which a user applied for

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,8 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { authGuard } from './core/auth-guard';
 import { TaskDetail } from './task-detail/task-detail';
-import { Home } from './home/home';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home, // public home route
-    },
     {
         path: 'dashboard',
         component: Dashboard,
@@ -18,9 +13,5 @@ export const routes: Routes = [
         path: 'tasks/:id',
         component: TaskDetail,
         canActivate: [authGuard],
-    },
-    {
-        path: '**',
-        redirectTo: '',
     },
 ];
