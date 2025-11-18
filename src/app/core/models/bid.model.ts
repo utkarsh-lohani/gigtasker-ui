af66eef9623a6a1b432fb18af92ff6db81e02d7a
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-11-18 22:51:26 +0530

    - Added Functionality to Mark a Task as complete by the bidder

--- src/app/core/models/bid.model.ts
+++ src/app/core/models/bid.model.ts
@@ -1,20 +1,25 @@
-import { TaskStatus } from './task.model';
+import { TaskStatusEnum } from "./task.model";
 
-export type BidStatus = 'PENDING' | 'ACCEPTED' | 'REJECTED' | 'CANCELLED';
+export enum BidStatusEnum {
+    PENDING = 'PENDING',
+    ACCEPTED = 'ACCEPTED',
+    REJECTED = 'REJECTED',
+    CANCELLED = 'CANCELLED',
+}
 
 export interface BidDTO {
     id: number;
     taskId: number;
     bidderUserId: number;
     amount: number;
-    status: BidStatus;
+    status: BidStatusEnum;
     proposal: string;
 }
 export interface BidDetailDTO {
     bidId: number;
     amount: number;
     proposal: string;
-    status: BidStatus;
+    status: BidStatusEnum;
     bidderUserId: number;
     bidderName: string;
 }
@@ -23,18 +28,18 @@ export interface MyBidDetailDTO {
     bidId: number;
     amount: number;
     proposal: string;
-    bidStatus: BidStatus;
+    bidStatus: BidStatusEnum;
     taskId: number;
     taskTitle: string;
-    taskStatus: TaskStatus;
+    taskStatus: TaskStatusEnum;
 }
 
 export interface MyBidDetailDTO {
     bidId: number;
     amount: number;
     proposal: string;
-    bidStatus: BidStatus;
+    bidStatus: BidStatusEnum;
     taskId: number;
     taskTitle: string;
-    taskStatus: TaskStatus;
+    taskStatus: TaskStatusEnum;
 }
