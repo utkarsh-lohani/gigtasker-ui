edc33b2a885b6e84db8e89cc841c23539dbe6c07
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-11-19 13:23:07 +0530

    - Added Changes to Show Bid Last Date and Other Information

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { authInterceptor } from './core/auth.interceptor';
+import { MatNativeDateModule } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZonelessChangeDetection(),
         provideRouter(routes),
         provideOAuthClient(),
-        provideHttpClient(withInterceptors([authInterceptor]))
+        provideHttpClient(withInterceptors([authInterceptor])),
+        importProvidersFrom([MatNativeDateModule])
     ]
 };
